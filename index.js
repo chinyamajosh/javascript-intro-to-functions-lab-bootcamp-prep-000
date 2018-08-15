e38bca087f40ff73a3b358009798ea4798d292fd
Joshua Chinyama <chinyamajosh@gmail.com>
2018-08-15 22:14:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,19 @@ function logWhisper(string) {
 
 logWhisper('HELLO')
 
+var lowercase = "hello!";
+var uppercase = "HELLO!";
 
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+  return `I can't hear you!`;
+}
+  if (string === uppercase) {
+  return `YES INDEED!`;
+}
+  if (string === "I love you, Grandma.") {
+  return `I love you, too.`;
+}
 
 
 

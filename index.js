a6b20b6a0cc7ec4292fd0bcfb9aac6ee56aa62ca
Joshua Chinyama <chinyamajosh@gmail.com>
2018-08-15 22:36:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ var lowercase
 var uppercase 
 
 function sayHiToGrandma(string){
-  if(string===lowercase);
+  if(string===lowercase)
   return "I can't hear you!"
 
-if(`string`===uppercase); 
+if(`string`===uppercase()); 
 return "YES INDEED!"
 
-if(`string` ==="I love you, Grandma.") 
+if(`string` ==="I love you, Grandma.")
 return "I love you, too"
 }
 

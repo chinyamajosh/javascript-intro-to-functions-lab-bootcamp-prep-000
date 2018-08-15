da76e4e09d4385fd4fa29a355e5cd3522fd2eae8
Joshua Chinyama <chinyamajosh@gmail.com>
2018-08-15 21:55:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+function shout(string) {
+return string.toUpperCase()
+ }
+
+shout('hello')
+
+function whisper(string) {
+  return string. toLowerCase()
+}
+
+whisper('HELLO')
+
+function logShout(string) {
+console.log(string. toUpperCase());
+}
+
+logShout('hello')
+
+function logWhisper(string) {
+  console.log(string. toLowerCase());
+}
+
+logWhisper('HELLO')
+
+var lowercase = "hello"
+var uppercase = "HELLO"
+
+function sayHiToGrandma(string) {
+  return string. toLowerCase()
+  if(string ===)
+  
+ 
+}
+
+sayHiToGrandma('hello')
+
+
+
+
+
+
+
